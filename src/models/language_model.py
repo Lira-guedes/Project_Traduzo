d33197ba5e405e3ce024b9b98837a5b43085b26f
Lira Guedes <liracrg@gmail.com>
2024-02-07 01:22:25 -0300

    feat: Lista os idiomas como dicionários

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -8,8 +8,19 @@ class LanguageModel(AbstractModel):
     def __init__(self, data: dict):
         super().__init__(data)
 
+    # cria um novo dicionário contendo os valores dos atributos name e acronym
+    # que será retornado como resultado da função
     def to_dict(self) -> dict:
         return {
             "name": self.data.get("name"),
             "acronym": self.data.get("acronym")
         }
+
+    # retorna uma lista de dicionários com todas as language no banco
+    @classmethod
+    def list_dicts(cls) -> list:
+        # itera sobre todas as languages da _collection retornadas pelo find()
+        # para cada language, cria uma instância de LanguageModel e chama
+        # o to_dict() para converter em um dicionário
+        # e os adiciona à lista de idiomas
+        return [cls(language).to_dict() for language in cls._collection.find()]
