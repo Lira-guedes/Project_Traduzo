6d3dd4d069107197c5143f29a30ab34cfba57171
Lira Guedes <liracrg@gmail.com>
2024-02-15 23:30:38 -0300

    test: Histórico de Traduções

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,12 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    history_json = HistoryModel.list_as_json()
+    assert json.loads(history_json)[0]["translate_to"] == "pt"
+    assert json.loads(history_json)[0]["translate_from"] == "en"
+    assert json.loads(
+        history_json
+        )[0]["text_to_translate"] == "Hello, I like videogame"
